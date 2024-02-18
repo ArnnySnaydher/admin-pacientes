2a1ec2797ae2b530370678827768d6beeb6c3173
Aryans <quispeloayza18@hotmail.com>
2024-02-17 23:05:49 -0500

    Editando Formulario con Tailwind

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,6 +8,38 @@
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
+
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <div class="mb-5">
+                <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+                <input id="mascota" type="text" placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+                <input id="propietario" type="text" placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
+                <input id="email" type="text" placeholder="Email del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+                <input id="alta" type="Date" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">Síntomas</label>
+                <textarea id="sintomas" placeholder="Describe los sintomas "
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"></textarea>
+            </div>
+
+            <input type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value="Registrar Paciente">
+        </form>
     </div>
 </template>
 
