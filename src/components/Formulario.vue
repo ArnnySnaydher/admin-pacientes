d643790f2f4fcf520a743b06dfd8e8585cb27142
Aryans <quispeloayza18@hotmail.com>
2024-02-17 23:27:12 -0500

    Usando reactive para capturar valores

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,10 @@
 <script setup>
+    import { reactive } from 'vue';
+
+    const paciente = reactive({
+        nombre:'',
+        propietario:''
+    })
 
 </script>
 <template>
@@ -10,15 +16,19 @@
         </p>
 
         <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            {{ paciente.nombre }}
+            {{ paciente.propietario }}
             <div class="mb-5">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
                 <input id="mascota" type="text" placeholder="Nombre de la mascota"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.nombre">
             </div>
             <div class="mb-5">
                 <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
                 <input id="propietario" type="text" placeholder="Nombre del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.propietario">
             </div>
             <div class="mb-5">
                 <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
